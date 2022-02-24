605dfb9c66c7c110f33c83fa3d77022e0c65af01
AneteBlazevica <anetebla94@gmail.com>
2022-02-24 14:52:30 +0200

    1

--- script.js
+++ script.js
@@ -9,7 +9,39 @@ function nolasa (m1,m2,m3){
         return{m1, m2, m3};
 
    } else {
-    console.log(false);
+    console.log(false); //ja mazāks pa 0, tad neveidojas
     return false
+}
+}
+
+function perimetrs(m1,m2,m3){
+    const m = nolasa();
+    m1=m.m1; m2=m.m2; m3=m.m3;
+    console.log(m1,m2,m3);
+    const p = m1+m2+m3;//saskaita, lai izrēķinātu perimetru
+    console.log(p);
+    return(p);
+}
+
+function pusper(m1,m2,m3){
+    const pusper = perimetrs(m1+m2+m3)/2; //izrēķina pusperimetru
+    console.log(pusper);
+    return(pusper);
+}
+
+function laukums(m1,m2,m3){
+    const pusper = perimetrs(m1+m2+m3)/2; //izrēķina pusperimetru
+    let tlaukums = Math.sqrt(pusper* ((pusper-m1) * (pusper-m2) * (pusper-m3))); //Hērona formula
+    console.log(tlaukums);
+    return tlaukums;
+}
+//Funkcija pārbauda vai divu malu summa ir lielāka par trešo malu
+function irTrijsturis(m1,m2,m3){
+    //pārbauda vai izpildās trijstūra nevienādīa
+    //atgriež true vai false
+if (m1<m2+m3 && m2<m1+m3 && m3<m1+m2){
+    return true;
+}else{
+    return false;
 }
 }
